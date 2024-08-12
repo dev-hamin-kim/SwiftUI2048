25abffb2049409b07d7eb3a1197b7e6fa602e875
Hamin Kim <dev.hamin.kim@gmail.com>
2024-08-12 16:41:14 +0900

    Initial Commit

---
+++ SwiftUI2048/SwiftUI2048App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI2048App.swift
+//  SwiftUI2048
+//
+//  Created by 김하민 on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI2048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
